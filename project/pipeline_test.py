7fae65c262435e286057b96a62fa89b18785c7ac
jiorgos2811 <gkesaris00@gmail.com>
2023-12-06 15:41:36 +0100

    Better commenting for test functions

--- project/pipeline_test.py
+++ project/pipeline_test.py
@@ -9,11 +9,14 @@ parent_dir = os.path.dirname(current_dir)
 db_path = os.path.join(parent_dir, "data", "UrbanAirQualityInMotion.sqlite")
 
 @pytest.mark.dependency()
-def test_data_retrieval(): 
+def test_data_retrieval():
+    """Check if the expected csv files were
+    downloaded and saved appropriately"""
+    
     data_pipeline = DataPipeline(link_traffic, link_airquality)
     data_pipeline.extract_data(data_pipeline.stations, data_pipeline.year)
     
-    # Check if the expected files are created in the data folder
+    # Check if the expected csv files are created in the data folder
     assert os.path.exists('./data/traffic_data.csv')
     for station in data_pipeline.stations:
         assert os.path.exists(f'./data/{station}.csv')
@@ -21,6 +24,8 @@ def test_data_retrieval():
 # Only execute test for the complete datapipeline if the previous tests passed
 @pytest.mark.dependency(depends=["test_data_retrieval"])
 def test_data_formating(): 
+    """Check if the sqlite db was created and if it contains the expected tables"""
+    
     data_pipeline = DataPipeline(link_traffic, link_airquality)
     csv_files = glob.glob(os.path.join(parent_dir, '*.csv'))
 
@@ -36,6 +41,7 @@ def test_data_formating():
         
     data_pipeline.run_data_pipeline()
 
+    #Check if the .sqlite file was created
     assert os.path.exists(db_path)
 
     engine = create_engine(f"sqlite:///{db_path}")
